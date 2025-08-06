223c800b1fb81fc1b5bf727a61deba7e8af8f3c5
omniaosama97 <omnyaosama0@gmail.com>
2025-08-06 15:03:02 +0300

    feat: Replace react-loader-spinner with react-spinners

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 import "./Loader.module.css"
-import { Bars } from 'react-loader-spinner'
+// import { Bars } from 'react-loader-spinner'
+import { PulseLoader } from "react-spinners"; 
 export default function Loader() {
   return (
-    <Bars
-  height="80"
-  width="80"
-  color="#4fa94d"
-  ariaLabel="bars-loading"
-  wrapperStyle={{}}
-  wrapperClass="h-screen flex justify-center items-center "
-  visible={true}
-  />
+  //   <Bars
+  // height="80"
+  // width="80"
+  // color="#4fa94d"
+  // ariaLabel="bars-loading"
+  // wrapperStyle={{}}
+  // wrapperClass="h-screen flex justify-center items-center "
+  // visible={true}
+  // />
+  <PulseLoader color={"#123abc"} loading={true} size={50} /> 
   )
 }
